90c9c5da5eba90ea04f0d829ac98010263639e94
Jordan <j_leble@yahoo.com>
2018-02-06 13:35:24 -0800

    Update Job

--- UrbanParks/src/model/Job.java
+++ UrbanParks/src/model/Job.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Job {
+import java.lang.IllegalArgumentException;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Job {
+	private static final int MAX_JOB_LENGTH = 3;
+	private static final int MILLIS_IN_DAY = 86400000;
+
+    private Calendar myStartDate;
+	private Calendar myEndDate;
+	
+	
+	public Job(int theStartMonth, int theStartDay, int theStartYear,
+				int theEndMonth, int theEndDay, int theEndYear) {
+		
+		Calendar myStartDate = new GregorianCalendar(theStartYear, theStartMonth, theStartDay);
+		Calendar myEndDate = new GregorianCalendar(theEndYear, theEndMonth, theEndDay);
+		
+		if((myEndDate.getTimeInMillis() - myStartDate.getTimeInMillis()) > MAX_JOB_LENGTH * MILLIS_IN_DAY) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
