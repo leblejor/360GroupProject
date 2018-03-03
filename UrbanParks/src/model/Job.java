701176af488aad1643bbd32a7b82e10c7e8105d5
Derick <derickzs@uw.edu>
2018-03-03 11:48:08 -0800

    fixes and createJobView

--- UrbanParks/src/model/Job.java
+++ UrbanParks/src/model/Job.java
@@ -158,7 +158,7 @@ public class Job implements java.io.Serializable {
 		Calendar today = Calendar.getInstance();
 		today.add(Calendar.DAY_OF_YEAR, theMaxScheduleWindow);
 		
-		if (today.compareTo(myEndDate) < 0) {
+		if (today.compareTo(myEndDate) < 0 || today.compareTo(myStartDate) < 0) {
 			conflict = true;
 		}
 		return conflict;
@@ -272,14 +272,14 @@ public class Job implements java.io.Serializable {
 		int startYear = myStartDate.get(Calendar.YEAR);
 		
 		// add 1 due to Calendar's implementation of months (Months starts at index 0)
-		int startMonth = myStartDate.get(Calendar.MONTH) + 1; 
+		int startMonth = myStartDate.get(Calendar.MONTH); 
 		
 		int startDay = myStartDate.get(Calendar.DATE);
 		
 		int endYear = myEndDate.get(Calendar.YEAR);
 		
 		// add 1 due to Calendar's implementation of months (Months starts at index 0)
-		int endMonth = myEndDate.get(Calendar.MONTH) + 1;
+		int endMonth = myEndDate.get(Calendar.MONTH);
 		
 		int endDay = myEndDate.get(Calendar.DATE);
 		
